b77d62c633e62ea583be91bf5a89286382278499
Anderson-Vilela-op <andersonlucash.al@gmail.com>
2026-05-18 16:08:30 -0300

    fix: navbar development

--- src/app/layouts/PublicLayout.tsx
+++ src/app/layouts/PublicLayout.tsx
@@ -1,11 +1,15 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import { Navbar } from "@shared/components/organisms/PublicNavbar";
 import { Footer } from "@shared/components/organisms/PublicFooter";
+import { siteConfig } from "@app/app/config/site";
 
 export function PublicLayout() {
+  const { pathname } = useLocation();
+  const hideNavbar = !siteConfig.isProd && pathname === "/";
+
   return (
     <div className="flex flex-col min-h-screen bg-bg antialiased">
-      <Navbar />
+      {!hideNavbar && <Navbar />}
       <main className="flex-1">
         <Outlet />
       </main>
